541f6448367bcb825cbb7423d488970cd3293a9b
oliv <mister.devo@gmail.com>
2016-02-14 16:57:10 +0100

    add e2eTest saucelab view

--- test/test-spec.js
+++ test/test-spec.js
@@ -14,20 +14,40 @@ describe('TFTFT page', function() {
     before(function(done){
           client = webdriverio.remote(options).init().call(done);
     });
-    
-    describe('verif title', function() {
-        
-        it('should have the right title - the fancy generator way', function (done) {
-            
+
+    describe('verif title on first page', function() {
+        it('should have the right title', function (done) {
             client
               .url('/')
-              .getTitle().then(function(title){
+              .getTitle()
+              .then(function(title){
                   assert.equal(title, 'TFTFT - Test For The First Time');
               })
               .call(done);
         });
     });
 
+    describe('saucelabs view', function() {
+        it('should display correct saucelabs link', function (done) {
+            client
+              .click('#project-link')
+              .getAttribute('#saucelabs-link','href')
+              .then(function(attr){
+                  assert.equal(attr,  options.baseUrl + '/#saucelabs');
+              })
+              .call(done);
+        });
+
+        it('should display saucelabs matrix with correct url', function (done) {
+            client
+              .url('/#saucelabs')
+              .getAttribute('#sl-img','src')
+              .then(function(attr){
+                  assert.equal(attr,'https://saucelabs.com/browser-matrix/misterdevo.svg');
+              })
+              .call(done);
+        });
+    });
 
     after(function(done) {
         client.end().call(done);
