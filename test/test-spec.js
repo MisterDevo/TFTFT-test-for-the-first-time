1613db5e4647c4d43a5d67b20f05d110cda4e421
oliv <mister.devo@gmail.com>
2016-02-16 16:03:01 +0100

    fix typo

--- test/test-spec.js
+++ test/test-spec.js
@@ -17,7 +17,7 @@ describe('TFTFT EndToEnd Test', function() {
           if(options.saucelabs){
             options.saucelabs.getJobs(function (err, jobs) {
               for (var k in jobs) {
-                if(job[k].build===options.build){
+                if(jobs[k].build===options.build){
                   client.sessionID = jobs[0].id;
                   break;
                 }
