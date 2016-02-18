b46c1c72c81dd91afff43686a2496725079b4508
oliv <mister.devo@gmail.com>
2016-02-18 11:47:10 +0100

    change unit tests

--- test/test.js
+++ test/test.js
@@ -23,38 +23,31 @@ describe('TFTFT Route Test', function() {
 
     describe('Covering routes/index', function () {
 
-        describe('GET /', function(){
-          it('respond with body burning', function(done){
+          it('GET / respond with body burning', function(done){
             request(app)
               .get('/')
               .expect('Content-Type', /html/)
               .expect(200)
               .expect(/<body class="burning">/, done);
           })
-        });
 
-        describe('GET /api/welcome', function(){
-          it('respond with json welcome messages', function(done){
+          it('GET /api/welcome respond with json welcome messages', function(done){
             request(app)
               .get('/api/welcome')
               .set('Accept', 'application/json')
               .expect('Content-Type', /json/)
               .expect(200, done);
           })
-        });
     });
 
     describe('Covering routes/users', function () {
 
-          describe('GET /users', function(){
-            it('respond with html', function(done){
+            it('GET /users respond with html', function(done){
               request(app)
                 .get('/users')
                 .expect('Content-Type', /html/)
                 .expect(200, done);
                 //.expect(/<body>/, done);
             })
-          });
     });
-
 });
