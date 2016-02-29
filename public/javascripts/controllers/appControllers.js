c87e7616084bb9fa8e81ec093abfa5dea9da4ed4
oliv <mister.devo@gmail.com>
2016-03-01 00:05:08 +0100

    try add globalController

--- public/javascripts/controllers/appControllers.js
+++ public/javascripts/controllers/appControllers.js
@@ -1,14 +1,19 @@
 angular.module('controllers', [])
 
-  .controller('welcomeController', ['$scope', '$injector', 
+  .controller('globalController', ['$scope', function($scope) {
+    		$scope.mochaCollapsed = true;
+	   }
+  ])
+
+  .controller('welcomeController', ['$scope', '$injector',
     function($scope, $injector) {
 	      require(['controllers/welcomeController'], function(ctrl) {
           			$injector.invoke(ctrl, this, {'$scope': $scope});
       	});
     }
   ])
-  
-  .controller('loginController', ['$scope', '$injector', 
+
+  .controller('loginController', ['$scope', '$injector',
     function($scope, $injector) {
 	      require(['controllers/loginController'], function(ctrl) {
           			$injector.invoke(ctrl, this, {'$scope': $scope});
