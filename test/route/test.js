f0411b323038b24e93a5696fda3416dc4e14d2f8
oliv <mister.devo@gmail.com>
2016-03-11 01:19:18 +0100

    route test for statics resources

--- test/route/test.js
+++ test/route/test.js
@@ -1,14 +1,43 @@
 var assert = require('assert');
 
 var request = require('supertest');
-
-//tested files
 var app = require('../../app');
-var index = require('../../routes/index');
-var users = require('../../routes/users');
-
 
 describe('TFTFT Route Test', function() {
+
+    describe('Statics files', function () {
+
+          it('must find Unit test mochawesome html report', function(done){
+            request(app)
+              .get('/report/test-unit.html')
+              .expect(200, done);
+          })
+
+          it('must find Route test mochawesome html report', function(done){
+            request(app)
+              .get('/report/test-route.html')
+              .expect(200, done);
+          })
+
+          it('must find End to End test mochawesome html report', function(done){
+            request(app)
+              .get('/report/test-spec.html')
+              .expect(200, done);
+          })
+
+          it('must find Coverage test istanbul html report', function(done){
+            request(app)
+              .get('/cov/lcov-report/index.html')
+              .expect(200, done);
+          })
+
+          it('must find Saucelab image', function(done){
+            request(app)
+              .get('/images/misterdevo.svg')
+              .expect(200, done);
+          })
+
+    });
 
     describe('Covering routes/index', function () {
 
@@ -29,6 +58,7 @@ describe('TFTFT Route Test', function() {
           })
     });
 
+
     describe('Covering routes/users', function () {
 
             it('GET /users respond with html', function(done){
@@ -37,6 +67,17 @@ describe('TFTFT Route Test', function() {
                 .expect('Content-Type', /html/)
                 .expect(200, done);
                 //.expect(/<body>/, done);
+            })
+    });
+
+
+    describe('Covering routes/mail', function () {
+
+            it('GET /mail respond with html', function(done){
+              request(app)
+                .get('/mail')
+                .expect('Content-Type', /html/)
+                .expect(200, done);
             })
     });
 });
