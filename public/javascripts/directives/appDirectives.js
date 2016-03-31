2a81ce1a597de87db7e9fbf2cc7fae56d3299112
oliv <mister.devo@gmail.com>
2016-03-31 23:24:29 +0200

    improve mailme directive

--- public/javascripts/directives/appDirectives.js
+++ public/javascripts/directives/appDirectives.js
@@ -7,15 +7,24 @@ angular.module('directives', [])
             }
         }
     })
-    .directive('mailme', function () {
+    .directive('mailme', ['$http', function($http) {
         return {
             restrict: 'E',
-            template: "<form id='mailForm'>"
-                      + "<input type='email' class='form-control' placeholder='Enter email or not !!!'>"
-                      + "<textarea class='form-control' rows='4' placeholder='Message ?'></textarea>"
-                      + "<a href='/mail' class='btn btn-primary form-control'>Send</a>"
+            template: "<form id='mailForm' name='mailForm' novalidate>"
+                      + "<input type='email' ng-model='user.email' class='form-control' placeholder='Enter email or not !!!'>"
+                      + "<textarea ng-model='user.message' required class='form-control' rows='4' placeholder='Message ?'></textarea>"
+                      + "<input type='submit' class='btn btn-primary form-control' ng-click='send()' value='Send'>"
+                      + "<div class='txtstyle'>{{response}}</div>"
                       + "</form>",
             link: function(scope, element, attrs) {
+                scope.send = function () {
+                  $http.post('/mail', scope.user)
+                      .then(function successCallback(response) {
+                          scope.response = response.data;
+                        }, function errorCallback(response) {
+                          scope.response = "An error occurs.";
+                      });
+              };
             }
         };
-    });
+    }]);
