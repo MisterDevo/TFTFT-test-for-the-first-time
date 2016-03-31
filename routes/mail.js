0f4261393ae9dc28bfaad302038d634dff747604
oliv <mister.devo@gmail.com>
2016-03-31 23:26:51 +0200

    route post mail with app message

--- routes/mail.js
+++ routes/mail.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  require('md-mailgun')( 'Excited User' , 'Wouhou Nice Mail', 'Crazy first mail from my app', function (statusCode) {
-      if(statusCode === 200) {
-         res.send('A mail has been send to administrator');
-      } else {
-         res.send('Sorry ... You are not authorize to send mail');
-      }
-  })
+router.post('/', function(req, res, next) {
+  if(req.body.message) {
+    require('md-mailgun')(
+          req.body.email || 'Unknown User',
+          'TFTFT Message',
+          req.body.message,
+          function (statusCode) {
+              if(statusCode === 200) {
+                 res.send('A mail has been send to administrator');
+              } else {
+                 res.send('Sorry ... You are not authorize to send mail');
+              }
+          })
+  } else {
+      res.status(500);
+      res.end();
+  }
 });
 
 module.exports = router;
