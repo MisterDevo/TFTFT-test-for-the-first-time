b5307b9f2eba626cc89cfd49be5f7f5bbc743ded
oliv <mister.devo@gmail.com>
2016-04-01 11:43:09 +0200

    add e2e test for mail feat

--- test/spec/test-wdjs-spec.js
+++ test/spec/test-wdjs-spec.js
@@ -246,6 +246,48 @@ test.describe('TFTFT End To End tests', function() {
 
     });
 
+    test.describe('Mail Me feature', function() {
+
+      test.it('should display mailme directive when click on mailme-link', function () {
+          client.findElement(webdriver.By.id('mailme-link')).click();
+          client.findElement(webdriver.By.css('mailme'))
+            .isDisplayed()
+            .then(function(displayed){
+                assert(displayed);
+            });
+       });
+
+       test.it('should return error if empty message is send', function () {
+         client.findElement(webdriver.By.css('mailme'))
+            .findElement(webdriver.By.css('form'))
+            .findElement(webdriver.By.css('input[type=submit]'))
+            .click().then(function () {
+               client.findElement(webdriver.By.css('.txtstyle'))
+               .getInnerHtml()
+               .then(function(html){
+                   assert.equal(html, 'An error occurs.');
+               });
+             })
+        });
+
+        test.it('should return response if message is send', function () {
+          var mailmeForm = client.findElement(webdriver.By.css('mailme'))
+                              .findElement(webdriver.By.css('form'));
+
+          mailmeForm.findElement(webdriver.By.css('textarea'))
+              .sendKeys("hello from selenium test");
+
+          mailmeForm.findElement(webdriver.By.css('input[type=submit]'))
+             .click().then(function () {
+                mailmeForm.findElement(webdriver.By.css('.txtstyle'))
+                .getInnerHtml()
+                .then(function(html){
+                    assert(html.length);
+                });
+              })
+         });
+    });
+
 
     var passed = true;
     test.afterEach(function() {
